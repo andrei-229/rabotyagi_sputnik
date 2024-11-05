bc0e8ad322911bc1276b9966806c3cdcc1bc82e4
Soslan <SoslanPar@yandex.ru>
2024-11-05 15:52:24 +0300

    Add files via upload
    add h(t) math model and comparsion

---
+++ comparsion_h(t).py
@@ -0,0 +1,24 @@
+import json
+import matplotlib.pyplot as plt
+
+with open('altitude_in_ksp.json') as f:
+    altitude_in_ksp = json.load(f)
+
+with open('altitude_math_model.json') as f:
+    altitude_math_model = json.load(f)
+
+with open('time_in_ksp.json') as f:
+    time = json.load(f)
+
+time = [time[i] - time[0] for i in range(len(time))]
+altitude_in_ksp = altitude_in_ksp[:len(time)]
+altitude_math_model = altitude_math_model[:len(time)]
+# print(len(time), len(speed_in_ksp), len(speed_math_model))
+
+plt.title('График высоты полёта ракеты от времени. Сравнение', fontsize=12, fontweight="bold")
+plt.ylabel("Высота h(t)", fontsize=14)
+plt.xlabel("Время t", fontsize=14)
+plt.grid()
+plt.plot(time, altitude_in_ksp, '-r', label='h(t) ksp')
+plt.plot(time, altitude_math_model, '-b', label='h(t) math')
+plt.show()
